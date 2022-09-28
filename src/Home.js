21cc8458639ac46cb76ce9957dd9dfdd075c076c
Kennedy Wardsworth <kennedywardsworth@Kennedys-MacBook-Pro.local>
2022-09-28 11:40:40 -0400

    Styling Changes

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import squirrel from "./resources/squirrel.jpg"
+import winterTree from  "./resources/winter-tree.jpg"
 
 /* 
   feel free to add/remove any HTML you would like. Just remember that all content
@@ -9,9 +10,9 @@ import squirrel from "./resources/squirrel.jpg"
 const Home = () => {
   return (
     <div className="app-content">
-      <h1>Welcome to this feature-rich test application</h1>
-      <h2>We're glad you're here</h2>
-      <img src={squirrel} alt="a cute little squirrel peering at you from the tall grass"></img>
+      <h1>Welcome to Kennedy's test application</h1>
+      <h2>I'm glad you're here</h2>
+      <img src={winterTree} alt="a tree covered in snow"></img>
     </div>
   );
 };
